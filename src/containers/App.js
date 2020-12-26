123943cc1d1d5bbd1cb15fad4b5ee2da3349ef89
Ivo Mujo <ivo.mujo.3@gmail.com>
2020-12-26 16:56:51 +0100

    adding files

---
+++ src/containers/App.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import './App.css';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: [],
+            searchfield: '',
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users=> this.setState({robots: users}))
+        
+    }
+
+    onSearchChanged = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render(){
+        const filteredRobots = this.state.robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        let noRobots = '';
+        if (!filteredRobots.length){
+            noRobots = 'No robots...'
+        }
+        if (this.state.robots.length === 0){
+            return <h1 className='tc'>Loading</h1>
+        }
+        else{
+            return(
+                <div className= 'tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange = {this.onSearchChanged}/>
+                    <Scroll>
+                        <ErrorBoundry>
+                            <p>{noRobots}</p>
+                            <CardList robots = {filteredRobots} />
+                        </ErrorBoundry>
+                    </Scroll>
+                </div>
+            );
+            } 
+        }
+    }
+
+
+export default App;
